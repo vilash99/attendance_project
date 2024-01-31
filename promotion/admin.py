ebb81a386adbb527d1dee49bfa4b64d2ccd608d3
vilash99 <vdaate@gmail.com>
2024-01-31 23:36:43 +0530

    blacklisted student forms

--- promotion/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Advertisement
-
-
-class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ('title', 'ads_url', 'image_url')
-
-
-admin.site.register(Advertisement, AdvertisementAdmin)
