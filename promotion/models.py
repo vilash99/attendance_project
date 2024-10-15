d4e78982308d9b2fd08d45db5f686556d3ad2430
vilash99 <vdaate@gmail.com>
2024-10-15 16:01:06 +0530

    Change size of promotions

--- promotion/models.py
+++ promotion/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Advertisement(models.Model):
-    title = models.CharField(max_length=100)
-    ads_url = models.CharField(max_length=100)
-    image_url = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    ads_url = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title
